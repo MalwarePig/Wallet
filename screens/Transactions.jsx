2e02e410cabbe4715727413f8331e249a5a0339c
Blackgeass <sergio_lcc92@hotmail.com>
2024-03-01 23:24:40 -0600

    first commit

--- screens/Transactions.jsx
+++ screens/Transactions.jsx
@@ -1,47 +1,102 @@
-import { View, Text, FlatList, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import DropDown from "../src/component/DropDown";
 import InputText from "../src/component/InputText";
+import { FontAwesome } from "@expo/vector-icons";
 
 const DATA = [
   {
-    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    id: "1",
     title: "First Item",
   },
   {
-    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    id: "2",
     title: "Second Item",
   },
   {
-    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    id: "3",
     title: "Third Item1",
   },
   {
-    id: "bd7acbea-c1b1-46c2-ae1d5-3ad53abb28ba",
+    id: "4",
     title: "First Item2",
   },
   {
-    id: "3ac68afc-c605-48d3-34f8-fbd91aa97f63",
+    id: "5",
     title: "Second Item3",
   },
   {
-    id: "58694a0f-3da1-471f-b4d96-145342571e29d72",
+    id: "6",
     title: "Third Iteassm4s",
   },
   {
-    id: "58694a0f-3da1-471f-b4d96-141235571e29d72",
+    id: "7",
+    title: "Third Item4123",
+  },
+  {
+    id: "8",
+    title: "First Item",
+  },
+  {
+    id: "9",
+    title: "Second Item",
+  },
+  {
+    id: "310",
+    title: "Third Item1",
+  },
+  {
+    id: "11",
+    title: "First Item2",
+  },
+  {
+    id: "15",
+    title: "Second Item3",
+  },
+  {
+    id: "16",
+    title: "Third Iteassm4s",
+  },
+  {
+    id: "17",
+    title: "Third Item4123",
+  },
+  {
+    id: "3130",
+    title: "Third Item1",
+  },
+  {
+    id: "131",
+    title: "First Item2",
+  },
+  {
+    id: "135",
+    title: "Second Item3",
+  },
+  {
+    id: "136",
+    title: "Third Iteassm4s",
+  },
+  {
+    id: "173",
     title: "Third Item4123",
   },
 ];
 
-const Item = ({title}) => (
+const Item = ({ title }) => (
   <View style={styles.item}>
     <Text style={styles.title}>{title}</Text>
   </View>
 );
 
 export default function Transactions() {
-  return (
+  return ( 
     <View style={styles.Container}>
       <Text style={styles.Title}> Transacciones</Text>
 
@@ -51,11 +106,22 @@ export default function Transactions() {
         <Text style={styles.TextLabel}>Monto $</Text>
         <InputText />
         <DropDown />
+      </View>
 
+      <View style={styles.ContainerList}>
         <FlatList
           data={DATA}
-          renderItem={({ item }) => <Item title={item.title} />}
-          keyExtractor={(item) => item.id}
+          renderItem={({ item, index }) => (
+            <View style={styles.Card}>
+              <View>
+                <FontAwesome name="cc-mastercard" size={24} color="black" />
+                <Text style={styles.TextLabel}>Tarjeta</Text>
+              </View>
+
+              <Text style={styles.TextLabel}>01/01/2024 $</Text>
+              <Text style={styles.TextLabel}>Monto $</Text>
+            </View>
+          )}
         />
       </View>
     </View>
@@ -66,13 +132,22 @@ const styles = StyleSheet.create({
   Container: {
     flex: 1,
     paddingHorizontal: 20,
+    marginTop: 40,
+  },
+  ContainerList: {
+    backgroundColor: "#ebebeb",
+    borderRadius: 15,
+    flex: 1,
+    paddingHorizontal: 15,
     marginTop: 20,
+    marginBottom:5,
+    paddingVertical:5
   },
   Title: {
     marginTop: 5,
     fontSize: 20,
     fontWeight: "bold",
-    textAlign: "center",
+    textAlign: "center", 
   },
   TextLabel: {
     fontSize: 15,
@@ -85,11 +160,25 @@ const styles = StyleSheet.create({
     marginTop: 10,
     flexDirection: "column",
     alignItems: "center",
-  }, 
+  },
   item: {
-    backgroundColor: '#f9c2ff',
+    backgroundColor: "#f9c2ff",
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
+  },
+  Card: {
+    backgroundColor: "#f9c2ff",
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",/* Alinecion Horizontal */
+    alignItems: "center",/* Alinecion Vertical */
+    
+    marginTop:5,
+    paddingHorizontal:10,
+    paddingVertical : 5,
+    backgroundColor: "#ebebeb",
+    borderRadius: 15,
+    borderWidth: 1,
   },
 });
